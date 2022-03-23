3f1b8ec199e54440d252f2ce5935b41face65648
Sergio <kurisergio777777@gmail.com>
2022-03-22 23:00:23 -0600

    commit tree d63ab59c8043cc22a1062d87573f30cc84af8723

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution here!
+
+const cats = [
+"Milo",
+"Otis",
+"Garfield"
+];
+
+function destructivelyAppendCat(cats){
+    cats.push("Ralph");
+    console.log(cats)
+
+};
+
+function destructivelyPrependCat( cats){
+    cats.unshift("pop");
+    console.log(cats)
+
+};
+
+
+   
+
+
